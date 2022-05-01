fb7207baef1478255e80f777110c81beff4678ca
Rafael <rafaelpaivavitor@hotmail.com>
2022-05-01 18:04:16 -0300

    add alert message

--- script.js
+++ script.js
@@ -3,16 +3,22 @@ async function req() {
     let data = [];
     let login = document.getElementById("login").value
 
-    await fetch(`https://api.github.com/users/${login}`)
+    if (login == "") {
+        alert("Preencha o campo")
+    } else {
 
-        .then(response => response.json())
-        .then(response => data = response) 
-        
+        await fetch(`https://api.github.com/users/${login}`)
+
+            .then(response => response.json())
+            .then(response => data = response)
+
         console.log(data)
 
-        const {name, avatar_url, bio} = data
-        
+        const { name, avatar_url, bio } = data
+
         document.getElementById("avatar").setAttribute("src", avatar_url);
         document.getElementById("name").innerText = name;
         document.getElementById("bio").innerText = bio;
     }
+
+}
