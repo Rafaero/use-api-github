dd9c436351c591c61df6b267c824163b919de2dd
Rafael <rafaelpaivavitor@hotmail.com>
2022-05-29 10:57:42 -0300

    update async function

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
 async function req() {
 
-    let data = [];
     let login = document.getElementById("login").value
 
     if (login == "") {
         alert("Preencha o campo")
     } else {
 
-       await fetch(`https://api.github.com/users/${login}`)
+        const data = await (await fetch(`https://api.github.com/users/${login}`)).json();
 
-            .then(response => response.json())
-            .then(response => data = response)
-            .catch(error => console.log(error))
-
-        console.log(data)
-        
         const { name, avatar_url, bio, message } = data
 
-        if(message == 'Not Found'){
+        if (message == 'Not Found') {
             alert("Usuario não encontrado")
-        }else{
+        } else {
             document.getElementById("avatar").setAttribute("src", avatar_url);
             document.getElementById("name").innerText = name;
             document.getElementById("bio").innerText = bio;
